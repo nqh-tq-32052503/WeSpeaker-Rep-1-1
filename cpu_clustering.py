6df16d661e7d07db21199746a78dfa360f2af49b
Nguyễn Quốc Hiệu <121285650+nqh-tq-32052503@users.noreply.github.com>
2025-12-19 10:17:21 +0700

    Update Clustering

---
+++ cpu_clustering.py
@@ -0,0 +1,26 @@
+import umap
+import hdbscan
+import copy
+import numpy as np
+
+class EmbeddingClustering(object):
+    def __init__(self):
+        self.clustering = hdbscan.HDBSCAN(allow_single_cluster=True, min_cluster_size=4, approx_min_span_tree=False, core_dist_n_jobs=1)
+        self.umap_params = {
+            "n_components" : 32,
+            "metric" : "cosine",
+            "n_neighbors" :  16,
+            "min_dist" : 0.05,
+            "random_state" : 2023,
+            "n_jobs" : 1
+        }
+    
+    def cluster(self, list_embeddings):
+        np_list_embeddings = [embedding.detach().cpu().numpy() for embedding in list_embeddings]
+        umap_params = copy.deepcopy(self.umap_params)
+        umap_params["n_components"] = min(32, len(np_list_embeddings) - 2)
+        umap_obj = umap.UMAP(**umap_params)
+        umap_embeddings = umap_obj.fit_transform(np.array(np_list_embeddings))
+        list_labels = self.clustering.fit_predict(umap_embeddings)
+        return list_labels
+    
