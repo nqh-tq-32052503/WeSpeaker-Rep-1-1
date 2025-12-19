9f9be94484841892fd1c1dcc58f4d88bbd913687
Nguyễn Quốc Hiệu <121285650+nqh-tq-32052503@users.noreply.github.com>
2025-12-19 09:28:52 +0700

    Update: add embedding module

--- fbank.py
+++ fbank.py
@@ -1,8 +1,36 @@
 import torchaudio.compliance.kaldi as kaldi
 import torch
+from tqdm import tqdm
 from lhotse.features import Fbank, FbankConfig
+from torch.func import vmap
 
+STANDARD_SEGMENT_DURATION = 5
+SAMPLE_RATE = 16000
+STANDARD_SEGMENT_LENGTH = int(STANDARD_SEGMENT_DURATION * SAMPLE_RATE)
+VMAP_FBANK = vmap(lambda w: kaldi.fbank(w.unsqueeze(0), num_mel_bins=80, frame_length=25, frame_shift=10, sample_frequency=16000, window_type='hamming'))
 EXTRACTOR = Fbank(FbankConfig(sampling_rate=16000, num_mel_bins=80, device="cuda"))
+WINDOW_OFFSET = torch.arange(STANDARD_SEGMENT_LENGTH).unsqueeze(0).to("cuda")
+
+def handle_short_segment(short_segment: torch.Tensor):
+    num_duplicates = int(STANDARD_SEGMENT_LENGTH / short_segment.shape[0]) + 1
+    duplicated_segment = torch.cat([short_segment] * num_duplicates, dim=0)
+    duplicated_segment = duplicated_segment[:STANDARD_SEGMENT_LENGTH].unsqueeze(0).to("cuda")
+    embedding = VMAP_FBANK(duplicated_segment)
+    return embedding
+
+def handle_long_segment(long_segment: torch.Tensor, stride=int(1 * SAMPLE_RATE)):
+    length = long_segment.size(0)
+    window_size = STANDARD_SEGMENT_LENGTH
+    max_start = length - window_size
+    stride_forward = window_size - stride
+    start_indices_forward = torch.arange(0, max_start + 1, stride_forward).to("cuda")
+    N1 = len(start_indices_forward)
+    # Bắt đầu indices: (N1 x 1) + (1 x W) = (N1 x W)
+    indices_N1 = start_indices_forward.unsqueeze(1) + WINDOW_OFFSET
+    # Trích xuất dữ liệu (N1 x W)
+    chunks_N1 = torch.index_select(long_segment, 0, indices_N1.flatten()).view(N1, window_size)
+    embedding_chunks = VMAP_FBANK(chunks_N1)
+    return embedding_chunks
 
 def extract_fbank_using_kaldi(waveform, normalize=True):
     waveform = waveform.to("cuda")
--- load_model.py
+++ load_model.py
@@ -3,12 +3,14 @@ from models.campplus import CAMPPlus
 from models.eres2net import ERes2Net34_aug
 from models.samresnet import SimAM_ResNet34_ASP
 from utils import load_checkpoint
-from fbank import extract_fbank
+from fbank import extract_fbank, handle_long_segment, handle_short_segment, STANDARD_SEGMENT_LENGTH
 import torch
+from tqdm import tqdm
 import yaml
 import os
 
 SUPPORTED_MODELS = ["campplus", "chinese", "english", "eres2net", "vblinkf", "vblinkp"]
+MAX_CHUNK_SIZE = 100
 
 class Model(object):
     def __init__(self, model_index, checkpoint_path, device="cuda"):
@@ -34,16 +36,50 @@ class Model(object):
         self.model.eval().to(self.device)
         print("Init model sucessfully")
     
-    def extract_embedding(self, waveform):
-        waveform = waveform.to(torch.float32).to(self.device)
-        feature = extract_fbank(waveform)
-        feature = feature.unsqueeze(0).to(self.device)
-        print("Input shape: ", feature.shape)
+    def __call__(self, list_segments):
+        input_segments = [(segment * (2 ** 15)).to(torch.float32).to(self.device) for segment in list_segments] # Mapping to 16-bit range instead of 0-1 normalization
+        short_segments = []
+        short_indices = []
+        long_segments = []
+        long_indices = []
+        for segment_index, segment in enumerate(input_segments):
+            if segment.size(0) >= STANDARD_SEGMENT_LENGTH:
+                long_segments.append(segment)
+                long_indices.append(segment_index)
+            else:
+                short_segments.append(segment)
+                short_indices.append(segment_index)
+        
+        long_fbanks = [handle_long_segment(segment) for segment in tqdm(long_segments, desc="Extracting FBank for long segments...")]
+        long_embeddings = [self.extract_embedding(long_fbank) for long_fbank in tqdm(long_fbanks, desc="Extracting Embeddings for long segments...")]
+        long_embeddings = [embedding.mean(dim=0) for embedding in long_embeddings]
+
+        short_fbanks = [handle_short_segment(segment) for segment in tqdm(short_segments, desc="Extracting FBank for short segments...")]
+        merged_short_fbanks = torch.cat(short_fbanks, dim=0)
+        N_short = merged_short_fbanks.size(0)
+        short_embeddings_list = []
+        for i in tqdm(range(0, N_short, MAX_CHUNK_SIZE), desc="Extracting Embeddings for short segments..."):
+            input_fbanks = merged_short_fbanks[i : i + MAX_CHUNK_SIZE]
+            output_embeddings = self.extract_embedding(input_fbanks)
+            short_embeddings_list.append(output_embeddings)
+        short_embeddings = torch.cat(short_embeddings_list, dim=0)
+
+        list_embeddings = [None] * len(list_segments)
+        for i, long_index in enumerate(long_indices):
+            list_embeddings[long_index] = long_embeddings[i]
+        for i, short_index in enumerate(short_indices):
+            list_embeddings[short_index] = short_embeddings[i]
+        return list_embeddings
+
+    def extract_embedding(self, input_fbanks):
+        # waveform = waveform.to(torch.float32).to(self.device)
+        # feature = extract_fbank(waveform)
+        # feature = feature.unsqueeze(0).to(self.device)
+        # print("Input shape: ", feature.shape)
         with torch.no_grad():
-            outputs = self.model(feature)
+            outputs = self.model(input_fbanks)
             outputs = outputs[-1] if isinstance(outputs, tuple) else outputs
-            embedding = outputs[0]
-            return embedding
+            return outputs
     
     def cosine_similarity(self, e1, e2):
         cosine_score = torch.dot(e1, e2) / (torch.norm(e1) * torch.norm(e2))
